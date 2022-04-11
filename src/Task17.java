f744de3326fed797dd7014aad28158b637640f2a
Soullloud <timofeysavchenko@icloud.com>
2022-04-11 19:15:31 +0300

    ready

---
+++ src/Task17.java
@@ -0,0 +1,18 @@
+import java.util.Scanner;
+
+public class Task17 {
+    public static void main(String[]args){
+        Scanner scan = new Scanner(System.in);
+        System.out.println("Ввести кол-во элементов: ");
+        int number = scan.nextInt();
+        int array[] = new int[number];
+        System.out.println("Ввести элементы массива: ");
+        for (int i = 0; i < number; i++){
+            array[i] = scan.nextInt();
+        }
+        System.out.println("Переворачиваем массив: ");
+        for (int i = array.length - 1; i >= 0; i--) {
+            System.out.println(array[i]);
+        }
+    }
+}
