20986ef8d0a6b6e2ed1f3b466148e628a405470c
Soullloud <97365214+Soullloud@users.noreply.github.com>
2022-04-27 21:44:29 +0300

    READY!

--- src/study/BeginString.java
+++ src/study/BeginString.java
@@ -7,10 +7,14 @@ public class BeginString {
         Scanner scanner = new Scanner(System.in);
         System.out.print("Input something text: ");
         String text = scanner.nextLine();
-        System.out.print(isPalindrome(text));
+        System.out.println(isPalindrome(text));
+        System.out.print("Input words: ");
+        String words = scanner.nextLine();
+        separator(words);
     }
-    static boolean isPalindrome(String text){
-        String s1 = text.toUpperCase().replaceAll("[^A-Za-z]"," " );
+
+    static boolean isPalindrome(String text) {
+        String s1 = text.toUpperCase().replaceAll("[^A-Za-z]", " ");
         StringBuffer s2 = new StringBuffer(s1).reverse();
         boolean isAnswer = true;
         for (int i = 0; i < s1.length(); i++) {
@@ -20,5 +24,12 @@ public class BeginString {
             }
         }
         return isAnswer;
+    }
+
+    static void separator(String text) {
+        String[] word = text.split(" ");
+        for (String words : word) {
+            System.out.println(words);
+        }
     }
 }
